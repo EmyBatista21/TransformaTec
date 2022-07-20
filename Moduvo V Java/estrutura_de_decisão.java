f001b82799ba03168ea14bf86466054383cb6c40
EmyBatista21 <emybatista159@gmail.com>
2022-07-20 18:59:56 -0300

    Trabalhando estruturas de decisão em java

---
+++ Moduvo V Java/estrutura_de_decisão.java
@@ -0,0 +1,11 @@
+public class estrutura_de_decisão {
+    public static void main(String[] args) {
+        boolean deMaior = false;
+        if(deMaior == true){
+            System.out.println("Pode beber um corote :)");
+        }else{
+            System.out.println("Apenas suco de groselha :(");
+        }
+    }
+    
+}
